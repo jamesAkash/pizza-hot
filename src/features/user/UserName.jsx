2170158b2b770f3f3a7d955cc67fff9bc8921818
jamesAkash <akash.james@outlook.com>
2024-03-14 15:17:26 +0530

    creating userSlice and store initial

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,7 +1,10 @@
+import { useSelector } from "react-redux";
+
 function Username() {
+  const username = useSelector((store) => store.user.username);
   return (
     <div className="hidden text-slate-100 text-sm font-semibold md:block">
-      Akash
+      {username}
     </div>
   );
 }
