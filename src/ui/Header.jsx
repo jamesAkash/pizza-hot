609b5a3e987de71dfb992d5defdf47d227ed6682
jamesAkash <akash.james@outlook.com>
2024-03-14 13:07:35 +0530

    Creating App Layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <Link to="/">Pizza🔥.Co</Link>
+      <p>Akash</p>
+    </header>
+  );
+};
+
+export default Header;
